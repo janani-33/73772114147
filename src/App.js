2faffd3066bfcda870469cb93165bd00a9e635a2
Janani S <janani.siva.117@gmail.com>
2023-12-03 02:27:44 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <div>Name : Janani S</div>
+          <div>Department : BE / CSE A</div>
+          <div>Year: 3rd Year</div>
+          <div>Register number : 73772114147</div>
+          <div>Area of interest : Software developer, tester.</div>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
